3a3c1203d7e3975836ca1323adfa12db151ba391
Prakash B R <51443131+BelluPr@users.noreply.github.com>
2021-03-30 15:03:43 +0530

    Program 18
    18.Find the power of the number without using existing methods

---
+++ Power.java
@@ -0,0 +1,25 @@
+/*18.Find the power of the number without using existing methods.*/
+
+import java.util.Scanner;
+public class Power {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the base Value");
+        int base = sc.nextInt();
+        System.out.println("Enter the exponent Value");
+        int exponent = sc.nextInt();
+
+        
+
+        long result = 1;
+
+        while (exponent != 0)
+        {
+            result *= base;
+            --exponent;
+        }
+
+        System.out.println("Answer = " + result);
+    }
+}
